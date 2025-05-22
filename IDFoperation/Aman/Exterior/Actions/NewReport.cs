0debe415f9c79d80b14ec61e09a6a7658a3298dd
berale <b.y.berale@gmail.com>
2025-05-22 09:12:09 +0300

    The general design diagram in code - before connections and algorithms

---
+++ IDFoperation/Aman/Exterior/Actions/NewReport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFoperation.Aman.ForeignIntelligence
+{
+    internal class NewReport
+    {
+        public Terorist terorist;
+        public string Location;
+        public string ReportTime;
+
+        public NewReport(
+            Terorist terorist,
+            string location,
+            string reportTime)
+        {
+            this.terorist = terorist;
+            Location = location;
+            ReportTime = reportTime;
+        }
+    }
+
+
+}
---
+++ IDFoperation/IDF/StrikeUinit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapons
+{
+
+    public abstract class StrikeUinit
+    {
+        public static List<StrikeUinit> idfWeapons = new List<StrikeUinit>();
+        public string CodeName;
+        public string Efficiency;
+        public int AvailableAttacks;
+
+        public StrikeUinit(string codeName)
+        {
+            this.CodeName = codeName;
+            idfWeapons.Add(this);
+           
+        }
+
+        public abstract void Attack();
+        
+    }
+
+    public class F16 : StrikeUinit
+    {
+        public F16(string codeName) : base(codeName)
+        { 
+            this.Efficiency = "structures";
+            this.AvailableAttacks = 8;
+            idfWeapons.Add(this);
+        }
+
+        public override void Attack()
+        {
+            AvailableAttacks--;
+        }
+    }
+        
+}
+
+
+
+
+
+
+
