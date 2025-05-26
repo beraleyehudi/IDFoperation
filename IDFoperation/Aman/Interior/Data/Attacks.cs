c9db3ad00c0259795c25548e4eafa9aece8e1f13
berale <b.y.berale@gmail.com>
2025-05-26 16:13:52 +0300

    123

--- IDFoperation/Aman/Interior/Data/Attacks.cs
+++ IDFoperation/Aman/Interior/Data/Attacks.cs
@@ -1,16 +1,20 @@
 ﻿using IDFoperation.Aman.Exterior.Actions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using IDFoperation.Aman.Exterior.Actions;
 
 namespace IDFoperation.Aman.Interior.Data
 
     {
     public static class Attacks
     {
-        private static List<OrderAttack> attacks = new List<OrderAttack>();
+        public static List<OrderAttack> attacks = new List<OrderAttack>();
+        public static OrderAttack last = null;
+
 
         public static void AddAttack(OrderAttack attack)
         {
@@ -19,6 +23,12 @@ namespace IDFoperation.Aman.Interior.Data
         public static List<OrderAttack> GetAttacks()
         {
             return attacks;
+        }
+
+        public static void Display()
+        {
+            //Console.WriteLine(last.);
+
         }
     }
 }
---
+++ IDFoperation/Controller/Menages.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDFoperation.AuxiliaryFunctions;
+using IDFoperation.Enemy;
+using IDFoperation.Aman.Interior.Data;
+using IDFoperation.Aman.Exterior.Actions;
+using IDFoperation.IDF;
+using IDFoperation.Aman.Exterior.Actions.Data;
+
+namespace IDFoperation.Controller
+{
+    public static class Menages
+    {
+       
+        public static void Menage(OperationsOfficer operationsOfficer)
+        {
+            bool exit;
+            Terrorist theTerrosit = null;
+            StrikeUinit strikeUinit = null;
+            do
+            {
+
+                exit = true;
+                string choice = All.PrintMenu("menu", new string[] { "The terrorist with the highest number of reports", "The most dangerous terrorist", "All attack tools"});
+
+                switch (choice)
+                {
+                    case "The terrorist with the highest number of reports":
+                        theTerrosit = All.MostReports();
+                        break;
+                       
+
+
+                    case "The most dangerous terrorist":
+                        theTerrosit = All.MostDangerous();
+                        break;
+                       
+
+
+                    case "All attack tools":
+                        Munitions.GetMunitionsData(All.typeStrike);
+                        exit = false;
+                        break;
+                }
+            } while (!exit);
+
+
+                    
+
+
+
+
+
+            strikeUinit = All.ChoosingAttackTool();
+
+            NewReport lastReport = Reports.reports[theTerrosit][Reports.reports[theTerrosit].Count - 1];
+
+            OrderAttack orderAttack = new OrderAttack
+                (
+                theTerrosit,
+                strikeUinit,
+                operationsOfficer,
+
+                lastReport
+
+                );
+            Console.WriteLine("secces");
+            strikeUinit.Attack();
+            return;
+
+        }
+
+    }
+}
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
